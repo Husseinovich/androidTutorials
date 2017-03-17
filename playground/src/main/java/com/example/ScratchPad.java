c090edf30f7d0cbe8bf3f4b4ce9fb9723ac4b88e
abdihusseinow@gmail.com <Husseinovich@Husseins-MacBook-Pro.local>
2017-03-17 17:45:02 -0400

    Added Playground Java Module to test out java classes

---
+++ playground/src/main/java/com/example/ScratchPad.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class ScratchPad {
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Hussein", "Mohamed", 33);
+        int age = student.getAge();
+
+        System.out.print(age);
+
+    }
+}
